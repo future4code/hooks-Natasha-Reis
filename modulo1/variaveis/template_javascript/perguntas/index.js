9fc2d2fb50e1fc356909a54648f4221c3e4ae47f
natashagomesr <natashagomesr@gmail.com>
2022-02-08 20:10:16 -0300

    Arquivos aula de variaveis

---
+++ modulo1/variaveis/template_javascript/perguntas/index.js
@@ -0,0 +1,18 @@
+
+let perguntaGostaCalor = "Você gosta de calor?"
+let perguntaRoupaRosa = "Você esta usando roupa rosa hoje?"
+let perguntaGostaFrio = "Você gosta de frio?"
+
+
+let repostaGostaCalor = prompt(perguntaGostaCalor)
+let repostaRoupaRosa = prompt(perguntaRoupaRosa)
+let repostaGostaFrio = prompt(perguntaGostaFrio)
+
+
+console.log(perguntaGostaCalor, "-", repostaGostaCalor)
+console.log(perguntaRoupaRosa, "-", repostaRoupaRosa)
+console.log(perguntaGostaFrio, "-", repostaGostaFrio)
+
+
+
+
