098dc54ef4e30c18bce198d84d31b706875364b0
Natasha Gomes <natashagomes@Karls-MacBook-Pro.local>
2022-11-10 21:23:11 -0300

    Inicio do projeto

---
+++ projeto-cookenu/src/business/UserBusiness.ts
@@ -0,0 +1,133 @@
+import { UserDatabase } from "../data/UserDatabase";
+import {
+  CustomError,
+  InvalidEmail,
+  InvalidName,
+  InvalidPassword,
+  Unauthorized,
+  UserNotFound,
+} from "../error/customError";
+import {
+  UserInputDTO,
+  user,
+  EditUserInputDTO,
+  EditUserInput,
+  LoginInputDTO,
+} from "../model/user";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
+import { TokenGenerator } from "../services/TokenGenerator";
+
+const idGenerator = new IdGenerator();
+const tokenGenerator = new TokenGenerator();
+const hashManager = new HashManager();
+const userDatabase = new UserDatabase();
+
+export class UserBusiness {
+  public createUser = async (input: UserInputDTO): Promise<string> => {
+    try {
+      const { name, email, password } = input;
+
+      if (!name || !email || !password) {
+        throw new CustomError(
+          400,
+          'Preencha os campos "name", "email" e "password"'
+        );
+      }
+
+      if (name.length < 4) {
+        throw new InvalidName();
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      const id: string = idGenerator.generateId();
+
+      const hashPassword: string = await hashManager.hash(password);
+
+      const user: user = {
+        id,
+        name,
+        email,
+        password: hashPassword,
+      };
+
+      await userDatabase.insertUser(user);
+      const token = tokenGenerator.generateToken(id);
+
+      return token;
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public login = async (input: LoginInputDTO): Promise<string> => {
+    try {
+      const { email, password } = input;
+
+      if (!email || !password) {
+        throw new CustomError(400, 'Preencha os campos "email" e "password"');
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      const user = await userDatabase.findUser(email);
+
+      if (!user) {
+        throw new UserNotFound();
+      }
+
+      const isValidPassword: boolean = await hashManager.compare(
+        password,
+        user.password
+      );
+
+      if (password.length < 6) {
+        throw new InvalidPassword();
+      }
+
+      if (!isValidPassword) {
+        throw new InvalidPassword();
+      }
+
+      const token = tokenGenerator.generateToken(user.id);
+
+      return token;
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public editUser = async (input: EditUserInputDTO) => {
+    try {
+      const { name, id, token } = input;
+
+      if (!name || !id || !token) {
+        throw new CustomError(400, 'Preencha os campos "id", "name"');
+      }
+
+      const data = tokenGenerator.tokenData(token);
+
+      if (!data.id) {
+        throw new Unauthorized();
+      }
+
+      if (name.length < 4) {
+        throw new InvalidName();
+      }
+
+      const editUserInput: EditUserInput = {
+        id,
+        name,
+      };
+
+      await userDatabase.editUser(editUserInput);
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+}
