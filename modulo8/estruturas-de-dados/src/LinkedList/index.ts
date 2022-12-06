004f6e959da928e870c563026918193495781263
Natasha Gomes <natashagomes@Karls-MacBook-Pro.local>
2022-12-06 20:16:36 -0300

    Estrutura-de-dados

---
+++ modulo8/estruturas-de-dados/src/LinkedList/index.ts
@@ -0,0 +1,9 @@
+import { LinkedList } from "./ListNode";
+
+const linkedList = new LinkedList(Node);
+
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+
+console.log(linkedList.find(1));
